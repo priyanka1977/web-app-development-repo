763edc19bd4827aeed20a37e978c4dfae986661f
priyanka gawada <devops-user1@outlook.com>
2022-11-07 15:51:25 +0530

    add greet.js & calculator.js

---
+++ calculator.js
@@ -0,0 +1,10 @@
+/*
+    add(num1, num2)
+    substract()
+    mul()
+    divide()
+   
+
+    Run this code in nodejs as well as browser
+
+*/
---
+++ greet.js
@@ -0,0 +1,24 @@
+/* 
+   This javascript function will greet the user
+   Function is a block of code with begin & code and needs 
+to be invoked for execution
+ 
+   
+
+function greet(){
+  return; // Optional
+}
+// invoke the function
+x = greet();
+*/
+function anonymous {
+
+  var x = 10;
+
+function greet() {
+  console.log("Welcome to javascript");
+}
+
+console.log("Outside the function");
+greet();
+}
